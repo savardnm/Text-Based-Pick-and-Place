422ce6fce3e0a1052c283c516fb048740cb30f48
Borzoneg <borzoneg@gmail.com>
2023-04-19 12:45:04 +0200

    Sorted the tree structure

--- utils.sh
+++
@@ -1,6 +0,0 @@
-
-# fix the Can't find model 'en_core_web_sm'. It doesn't seem to be a Python package or a valid path to a data directory. error
-pip3 install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.2.0/en_core_web_sm-2.2.0.tar.gz
-# to be run for fixing CLIP 
-pip install ftfy regex tqdm
-pip install git+https://github.com/openai/CLIP.git
